133344004a89577b0fa49be2d098f92bd5d74559
Christian Cosgrove <christian.cosgrove@gmail.com>
2018-07-14 17:08:53 -0400

    implement PyTorch BSDS dataset class

--- bsds_dataset.py
+++ bsds_dataset.py
@@ -1,147 +1,38 @@
 import os
+from typing import List
+
 import numpy as np
-from PIL import Image
-from skimage.util import img_as_float
-from skimage.color import rgb2grey
-from skimage.io import imread
-from scipy.io import loadmat
+from scipy.misc import imresize
+from tensorflow.python.data import Dataset
+
+from bsds_wrapper import BSDSWrapper
 
 
-class BSDSDataset (object):
-    """
-    BSDS dataset wrapper
+class BSDSDataset(Dataset):
 
-    Given the path to the root of the BSDS dataset, this class provides
-    methods for loading images, ground truths and evaluating predictions
+    labels: List[np.ndarray]
+    images: List[np.ndarray]
+    wrapper: BSDSWrapper
 
-    Attribtes:
+    def __init__(self, data_dir: str):
+        images_dir = os.path.join(data_dir, "BSDS500/data/images")
+        ground_truth_dir = os.path.join(data_dir, "BSDS500/data/groundTruth")
 
-    bsds_path - the root path of the dataset
-    data_path - the path of the data directory within the root
-    images_path - the path of the images directory within the data dir
-    gt_path - the path of the groundTruth directory within the data dir
-    train_sample_names - a list of names of training images
-    val_sample_names - a list of names of validation images
-    test_sample_names - a list of names of test images
-    """
-    def __init__(self, bsds_path):
-        """
-        Constructor
+        self.wrapper = BSDSWrapper(data_dir)
+        self.images = []
+        self.labels = []
 
-        :param bsds_path: the path to the root of the BSDS dataset
-        """
-        self.bsds_path = bsds_path
-        self.data_path = os.path.join(bsds_path, 'BSDS500', 'data')
-        self.images_path = os.path.join(self.data_path, 'images')
-        self.gt_path = os.path.join(self.data_path, 'groundTruth')
+        for image in self.wrapper.train_sample_names:
+            x = np.array(self.wrapper.read_image(image), dtype=np.float) / 256.0
+            x = imresize(x, (320, 320)).reshape((320, 320, 3))
+            x = x.transpose(2, 0, 1)
+            y = np.array(self.wrapper.load_boundaries(os.path.join(ground_truth_dir, image))[0], dtype=np.float)
+            y = imresize(y, (320, 320)).reshape((320, 320))
+            self.images.append(x)
+            self.labels.append(y)
 
-        self.train_sample_names = self._sample_names(self.images_path, 'train')
-        self.val_sample_names = self._sample_names(self.images_path, 'val')
-        self.test_sample_names = self._sample_names(self.images_path, 'test')
+    def __getitem__(self, index):
+        return self.images[index], self.labels[index]
 
-    @staticmethod
-    def _sample_names(dir, subset):
-        names = []
-        files = os.listdir(os.path.join(dir, subset))
-        for fn in files:
-            dir, filename = os.path.split(fn)
-            name, ext = os.path.splitext(filename)
-            if ext.lower() == '.jpg':
-                names.append(os.path.join(subset, name))
-        return names
-
-    def read_image(self, name):
-        """
-        Load the image identified by the sample name (you can get the names
-        from the `train_sample_names`, `val_sample_names` and
-        `test_sample_names` attributes)
-        :param name: the sample name
-        :return: a (H,W,3) array containing the image, scaled to range [0,1]
-        """
-        path = os.path.join(self.images_path, name + '.jpg')
-        return img_as_float(imread(path))
-
-    def get_image_shape(self, name):
-        """
-        Get the shape of the image identified by the sample name (you can
-        get the names from the `train_sample_names`, `val_sample_names` and
-        `test_sample_names` attributes)
-        :param name: the sample name
-        :return: a tuple of the form `(height, width, channels)`
-        """
-        path = os.path.join(self.images_path, name + '.jpg')
-        img = Image.open(path)
-        return img.height, img.width, 3
-
-    def ground_truth_mat(self, name):
-        """
-        Load the ground truth Matlab file identified by the sample name
-        (you can get the names from the `train_sample_names`,
-        `val_sample_names` and `test_sample_names` attributes)
-        :param name: the sample name
-        :return: the `groundTruth` entry from the Matlab file
-        """
-        path = os.path.join(self.gt_path, name + '.mat')
-        return self.load_ground_truth_mat(path)
-
-    def segmentations(self, name):
-        """
-        Load the ground truth segmentations identified by the sample name
-        (you can get the names from the `train_sample_names`,
-        `val_sample_names` and `test_sample_names` attributes)
-        :param name: the sample name
-        :return: a list of (H,W) arrays, each of which contains a
-        segmentation ground truth
-        """
-        path = os.path.join(self.gt_path, name + '.mat')
-        return self.load_segmentations(path)
-
-    def boundaries(self, name):
-        """
-        Load the ground truth boundaries identified by the sample name
-        (you can get the names from the `train_sample_names`,
-        `val_sample_names` and `test_sample_names` attributes)
-        :param name: the sample name
-        :return: a list of (H,W) arrays, each of which contains a
-        boundary ground truth
-        """
-        path = os.path.join(self.gt_path, name + '.mat')
-        return self.load_boundaries(path)
-
-    @staticmethod
-    def load_ground_truth_mat(path):
-        """
-        Load the ground truth Matlab file at the specified path
-        and return the `groundTruth` entry.
-        :param path: path
-        :return: the 'groundTruth' entry from the Matlab file
-        """
-        gt = loadmat(path)
-        return gt['groundTruth']
-
-    @staticmethod
-    def load_segmentations(path):
-        """
-        Load the ground truth segmentations from the Matlab file
-        at the specified path.
-        :param path: path
-        :return: a list of (H,W) arrays, each of which contains a
-        segmentation ground truth
-        """
-        gt = BSDSDataset.load_ground_truth_mat(path)
-        num_gts = gt.shape[1]
-        return [gt[0,i]['Segmentation'][0,0].astype(np.int32) for i in range(num_gts)]
-
-    @staticmethod
-    def load_boundaries(path):
-        """
-        Load the ground truth boundaries from the Matlab file
-        at the specified path.
-        :param path: path
-        :return: a list of (H,W) arrays, each of which contains a
-        boundary ground truth
-        """
-        gt = BSDSDataset.load_ground_truth_mat(path)
-        num_gts = gt.shape[1]
-        return [gt[0,i]['Boundaries'][0,0] for i in range(num_gts)]
-
+    def __len__(self):
+        return len(self.images)
--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-import os
-import numpy as np
-import torch
-from scipy.misc import imresize
-from torch.autograd import Variable
-
-from bsds_dataset import BSDSDataset
+from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
-
+from bsds_dataset import BSDSDataset
+from bsds_wrapper import BSDSWrapper
 from unet import UNet
+
+
+def get_training_batch(dataset: BSDSWrapper, batch_size: int):
+    dataset.load_boundaries()
+
 
 if __name__ == "__main__":
     """
@@ -15,27 +15,38 @@ if __name__ == "__main__":
     """
     model = UNet(3, depth=5, merge_mode='concat')
     data_dir = "./data/BSR"
-    images_dir = os.path.join(data_dir, "BSDS500/data/images")
-    groundtruth_dir = os.path.join(data_dir, "BSDS500/data/groundTruth")
 
-    dataset = BSDSDataset(data_dir)
+    dset = BSDSDataset(data_dir)
+    mb_size = 4
+    loader = DataLoader(dset, batch_size=mb_size)
 
-    for image in dataset.train_sample_names:
-        x = dataset.read_image(image)
-        y = dataset.load_boundaries(os.path.join(groundtruth_dir, image))
+    for x, y in loader:
 
-        x = imresize(x, (320, 320)).reshape((1, 320, 320, 3))
-        x = x.transpose(0, 3, 1, 2)
+        out_cpu = x.detach().numpy().transpose(0, 2, 3, 1).reshape(mb_size, 320, 320, 3)
+        out_cpu_y = y.detach().numpy().reshape(mb_size, 320, 320)
 
-        tensor = Variable(torch.FloatTensor(np.random.random((1, 3, 320, 320))))
-        out = model(tensor)
-        # loss = torch.sum(out)
-        # loss.backward()
-
-        out_cpu = out.detach().numpy().transpose(0, 2, 3, 1).reshape(320, 320, 3)
-
-        plt.imshow(out_cpu)
+        plt.imshow(out_cpu[0])
         plt.show()
-        plt.imshow(y[0])
+
+        plt.imshow(out_cpu_y[0])
         plt.show()
+
+    # for image in dataset.train_sample_names:
+    #     x = dataset.read_image(image)
+    #     y = dataset.load_boundaries(os.path.join(groundtruth_dir, image))
+    #
+    #     x = imresize(x, (320, 320)).reshape((1, 320, 320, 3))
+    #     x = x.transpose(0, 3, 1, 2)
+    #
+    #     tensor = Variable(torch.FloatTensor(np.random.random((1, 3, 320, 320))))
+    #     out = model(tensor)
+    #     # loss = torch.sum(out)
+    #     # loss.backward()
+    #
+    #     out_cpu = out.detach().numpy().transpose(0, 2, 3, 1).reshape(320, 320, 3)
+    #
+    #     plt.imshow(out_cpu)
+    #     plt.show()
+    #     plt.imshow(y[0])
+    #     plt.show()
 
