624215c4e4bdd7e0697e96e4f1fec53dc534b951
Christian Cosgrove <christian.cosgrove@gmail.com>
2018-09-02 19:42:45 -0400

    preliminary fgsm

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ from bsds import evaluate_boundaries
 import tqdm
 from torch.nn import functional as F
 
+
 def get_training_batch(dataset: BSDSWrapper, batch_size: int):
     dataset.load_boundaries()
 
@@ -25,19 +26,29 @@ parser.add_argument("--checkpoint_dir", type=str, default="checkpoint")
 parser.add_argument("--load", help="load or not", action="store_true")
 args = parser.parse_args()
 
+
 def load_checkpoint(checkpoint_dir: str, mod: torch.nn.Module, optim: torch.optim.Optimizer):
     checkpoint = torch.load(checkpoint_dir)
-    mod.load_state_dict(checkpoint['state_dict'])
+    mod.load_state_dict(checkpoint['checkpoint'])
     optim.load_state_dict(checkpoint['optimizer'])
 
 
+def save_checkpoint(filename: str, model: torch.nn.Module, optim: torch.optim.Optimizer):
+    check = {}
+    check['state_dict'] = model.state_dict()
+    check['optimizer'] = optim.state_dict()
+    torch.save(check, filename)
+
+
 """ Divides an image into 320x320 patches to be fed to the U-Net model."""
+
+
 def divide_image(image: np.ndarray, num_channels: int):
     expanded = np.tile(image, (1, 2, 2))
     h = expanded.shape[1]
     w = expanded.shape[2]
 
-    assert(w >= 320 and h >= 320)
+    assert (w >= 320 and h >= 320)
 
     nh = int(np.ceil(image.shape[1] / 320)) * 320
     nw = int(np.ceil(image.shape[2] / 320)) * 320
@@ -47,12 +58,15 @@ def divide_image(image: np.ndarray, num_channels: int):
     return np.reshape(
         np.transpose(
             np.reshape(expanded, (num_channels, nh // 320, 320, nw // 320, 320)),
-                (1, 3, 0, 2, 4)
+            (1, 3, 0, 2, 4)
         ), (-1, num_channels, 320, 320)), nh // 320, nw // 320
 
 
 """ Combines the output of a U-Net model (multiple 320x320 patches) back into the original-sized image. """
-def combine_images(repeats_x: int, repeats_y: int, original_width: int, original_height: int, image: np.ndarray, num_channels: int):
+
+
+def combine_images(repeats_x: int, repeats_y: int, original_width: int, original_height: int, image: np.ndarray,
+                   num_channels: int):
     return np.reshape(
         np.transpose(
             np.reshape(image,
@@ -60,9 +74,12 @@ def combine_images(repeats_x: int, repeats_y: int, original_width: int, original
             (0, 1, 3, 2, 4)),
         (num_channels, repeats_y * 320, repeats_x * 320))[:, :original_height, :original_width]
 
+
 """ Gets the output of the model on an arbitrarily-sized image.
     Uses the divide_image routine to process the image in patches.
  """
+
+
 def get_model_output(model: UNet, image: np.ndarray):
     original_height = image.shape[1]
     original_width = image.shape[2]
@@ -72,7 +89,6 @@ def get_model_output(model: UNet, image: np.ndarray):
     out = F.sigmoid(model(tensor_x))
     combined_out = combine_images(rx, ry, original_width, original_height, out.cpu().detach().numpy(), 1)
     return combined_out
-
 
 
 def print_results(sample_results, threshold_results, overall_result):
@@ -93,6 +109,7 @@ def print_results(sample_results, threshold_results, overall_result):
         overall_result.threshold, overall_result.recall, overall_result.precision, overall_result.f1,
         overall_result.best_recall, overall_result.best_precision, overall_result.best_f1,
         overall_result.area_pr))
+
 
 def precision_recall_chart(threshold_results, title: str):
     prec = []
@@ -117,7 +134,8 @@ def evaluate(model: UNet, dset: BSDSDataset, iteration: int):
         return dset.labels[image]
 
     sample_results, threshold_results, overall_result = \
-        evaluate_boundaries.pr_evaluation(20, dset.wrapper.test_sample_names, load_gt_boundary, load_prediction, progress=tqdm.tqdm)
+        evaluate_boundaries.pr_evaluation(20, dset.wrapper.test_sample_names, load_gt_boundary, load_prediction,
+                                          progress=tqdm.tqdm)
 
     precision_recall_chart(threshold_results, 'iteration_{}'.format(iteration))
 
@@ -125,21 +143,72 @@ def evaluate(model: UNet, dset: BSDSDataset, iteration: int):
 
 
 def disp_image_output(x: np.ndarray, y: np.ndarray, out: np.ndarray):
-    out_reshaped = np.squeeze(out)
-    x_reshaped = np.transpose(x, (1, 2, 0))
-    y_reshaped = np.squeeze(y)
+    disp_image_single(x)
+    disp_edge_single(y)
+    disp_edge_single(y)
 
+
+def disp_image_single(x: np.ndarray, title=None):
+    x_reshaped = np.transpose(x, (1, 2, 0))
     plt.imshow(x_reshaped * 256)
+    if title is not None:
+        plt.title(title)
     plt.show()
+
+
+def disp_edge_single(y: np.ndarray, title=None):
+    y_reshaped = np.squeeze(y)
     plt.imshow(y_reshaped)
+    if title is not None:
+        plt.title(title)
     plt.show()
-    plt.imshow(out_reshaped * 256)
-    plt.show()
+
+
+def attack(model: UNet, x: np.ndarray, target_y: torch.Tensor, groundtruth_y: torch.Tensor):
+    x_adv = np.copy(x)
+    step_size = 0.00001
+    epsilon = 0.000001
+
+    iterations = 50
+
+    for i in range(iterations):
+        x_adv = torch.Tensor(x_adv).cuda()
+        x_adv.requires_grad = True
+        out = model(x_adv).cuda()
+
+        #adversarial target
+        adv_target = 1 - groundtruth_y
+
+        loss = BCEWithLogitsLoss()(out, adv_target)
+        loss.backward()
+        grad = x_adv.grad
+        grad = grad.cpu().detach().numpy()
+        x_adv = x_adv.cpu().detach().numpy()
+        x_adv = np.add(x_adv, step_size * np.sign(grad))
+        x_adv = np.clip(x_adv, x - epsilon, x + epsilon)
+        x_adv = np.clip(x_adv, 0, 1)
+
+    return x_adv
+
+
+def attack_and_display(model: UNet, x: torch.Tensor, y: np.ndarray, target_y: torch.Tensor, model_out: torch.Tensor):
+    plt.close(plt.figure())
+    attack_out = attack(model, x[:1], target_y[:1], y[:1])
+    disp_image_single(x[0], 'Real Input')
+    disp_image_single(attack_out[0], 'Attacked Input')
+
+    # Compute model output on adversarial input
+
+    adv_out = model(torch.Tensor(attack_out).cuda())
+
+    disp_edge_single(adv_out[0].cpu().detach().numpy(), 'Adversarial Output')
+    disp_edge_single(model_out[0].cpu().detach().numpy(), 'Model Output')
+    disp_edge_single(target_y[0].cpu().detach().numpy(), 'Target Output')
+    disp_edge_single(y[0].cpu().detach().numpy(), 'Groundtruth Output')
 
 
 def train():
-
-    model = UNet(num_classes=1, depth=3, merge_mode='concat').cuda()
+    model = UNet(num_classes=1, depth=1, start_filts=16, merge_mode='concat').cuda()
 
     data_dir: str = "../BSR"
 
@@ -162,6 +231,9 @@ def train():
             x = x.float().cuda()
             y = y.float().cuda()
 
+            if iteration == 0:
+                target_y = y[:1]
+
             out = model(x).cuda()
 
             optimizer.zero_grad()
@@ -170,16 +242,17 @@ def train():
 
             optimizer.step()
 
-
             if iteration % 1000 == 999:
-                torch.save(model.state_dict(), os.path.join(args.checkpoint_dir, "checkpoint_{}".format(iteration)))
+                save_checkpoint(os.path.join(args.checkpoint_dir, "checkpoint_{}".format(iteration)), model, optimizer)
 
             iteration += 1
 
-            if iteration % 1000 == 10:
-                print('iteration {}; loss {}'.format(iteration, loss.cpu().detach().numpy()))
-                evaluate(model, test_dset, iteration)
+            if iteration % 1000 == 250:
+                attack_and_display(model, x, y, target_y, out)
+            # if iteration % 1000 == 10:
+            #     print('iteration {}; loss {}'.format(iteration, loss.cpu().detach().numpy()))
+            #     evaluate(model, test_dset, iteration)
+
 
 if __name__ == "__main__":
     train()
-
